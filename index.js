c6a230301de6672b54cc65dcc070f77dd2c99880
kastet <kastet@weareclario.com>
2026-01-07 13:03:58 +0200

    feat: Add API response time logging to fetch requests.

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const interval = 25; // sec
 setInterval(() => {
   console.log(`Executing task with ${interval}s interval`);
 
+  const startTime = Date.now();
   fetch("https://api.vdovareize.me/track")
     .then((res) => res.json())
     .then((data) => console.log("API Result:", data))
-    .catch((err) => console.error("Fetch error:", err));
+    .catch((err) => console.error("Fetch error:", err))
+    .finally(() => console.log("Response Time:", Date.now() - startTime, "ms"));
 }, interval * 1000);
 app.get("/", (req, res) => {
   res.send("Server is running. Cron is active.");
