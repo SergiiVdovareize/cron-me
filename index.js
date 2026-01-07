17fc6ffe48ab86f814947488e82469f38c374a6e
kastet <kastet@weareclario.com>
2026-01-07 12:23:06 +0200

    fix: Change interval unit from milliseconds to seconds and adjust its usage.

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const interval = 30000;
+const interval = 25; // sec
 setInterval(() => {
   console.log(
-    `[${new Date().toISOString()}] Executing second task with ${interval / 1000} interval...`
+    `[${new Date().toISOString()}] Executing second task with ${interval} interval...`
   );
 
   // Example:
@@ -13,7 +13,7 @@ setInterval(() => {
     .then((res) => res.json())
     .then((data) => console.log("API Result:", data))
     .catch((err) => console.error("Fetch error:", err));
-}, interval);
+}, interval * 1000);
 app.get("/", (req, res) => {
   res.send("Server is running. Cron is active.");
 });
