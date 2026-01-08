2bc1c5ebaa3a89b1304914c3f527c5983765239c
Sergii Vdovareize <servdo@gmail.com>
2026-01-08 23:30:51 +0200

    tracking

--- index.js
+++ index.js
@@ -2,17 +2,29 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const interval = 25; // sec
-setInterval(() => {
-  console.log(`Executing task with ${interval}s interval`);
+const interval = 20; // sec
 
-  const startTime = Date.now();
-  fetch("https://api.vdovareize.me/track")
-    .then((res) => res.json())
-    .then((data) => console.log("API Result:", data))
-    .catch((err) => console.error("Fetch error:", err))
-    .finally(() => console.log("Response Time:", Date.now() - startTime, "ms"));
-}, interval * 1000);
+function startTracking() {
+  try {
+    console.log(`Executing task in ${interval}s after previous response.`);
+
+    const startTime = Date.now();
+    fetch("https://api.vdovareize.me/track")
+      .then((res) => res.json())
+      .then((data) => console.log("API Result:", data))
+      .catch((err) => console.error("Fetch error:", err))
+      .finally(() => {
+        console.log("Response Time:", Date.now() - startTime, "ms");
+        setTimeout(startTracking, interval * 1000);
+      });
+  } catch (err) {
+    console.error("Tracking error:", err);
+    setTimeout(startTracking, interval * 1000);
+  }
+}
+
+startTracking();
+
 app.get("/", (req, res) => {
   res.send("Server is running. Cron is active.");
 });
