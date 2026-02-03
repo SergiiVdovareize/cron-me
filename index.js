289bb8930f8a6e5c340ce8a437d7d94c4c400d0f
Sergii Vdovareize <servdo@gmail.com>
2026-02-04 00:01:47 +0200

    feat: Add a new daily task to nudge the cache and its corresponding interval.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const interval = 48; // sec
+const dailyInterval = 24 * 60 * 60; // 24 hours
 
 function startTracking() {
   try {
@@ -23,7 +24,27 @@ function startTracking() {
   }
 }
 
+function startDailyTask() {
+  try {
+    console.log(`Executing daily nudge task`);
+
+    const startTime = Date.now();
+    fetch("https://api.vdovareize.me/cache/nudge")
+      .then((res) => res.json())
+      .then((data) => console.log("API Result:", data))
+      .catch((err) => console.error("Fetch error:", err))
+      .finally(() => {
+        console.log("Response Time:", Date.now() - startTime, "ms");
+        setTimeout(startDailyTask, dailyInterval * 1000);
+      });
+  } catch (err) {
+    console.error("Daily task error:", err);
+    setTimeout(startDailyTask, dailyInterval * 1000);
+  }
+}
+
 startTracking();
+startDailyTask();
 
 app.get("/", (req, res) => {
   res.send("Server is running. Cron is active.");
